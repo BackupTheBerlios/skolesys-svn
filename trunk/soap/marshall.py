960a6192cf96e737c90f104b0061231f7afba24c
jakob@refsvindinge.dk <jakob@refsvindinge.dk@f5392857-ef32-0410-a601-84722a433970>
2006-07-21 17:30:38 +0000

    Marshall with base64

--- trunk/soap/marshall.py
+++ trunk/soap/marshall.py
@@ -2,7 +2,7 @@ import pickle
 import base64
 
 def pdump(obj):
-    return pickle.dumps(obj)
+    return base64.b64encode(pickle.dumps(obj))
 
 def pload(obj):
-    return pickle.loads(obj)
+    return pickle.loads(base64.b64decode(obj))
