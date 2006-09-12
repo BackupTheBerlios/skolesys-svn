817e3a22e651129260aefd592c274f44a9713a01
skolesys-devel <skolesys-devel@f5392857-ef32-0410-a601-84722a433970>
2006-09-12 21:30:40 +0000

    added LTSP module and iso source support

---
+++ trunk/buildtools/skolesys-deb/skolesys-ui_debinfo.py
@@ -0,0 +1,42 @@
+fetch_method = "svn"
+
+prebuild_script = \
+"""
+import os
+import re
+
+rx_uifile = re.compile('^(.*)\.ui$')
+
+def ui_filter(args,dirname,names):
+        for n in names:
+                m = rx_uifile.match(n)
+                if m:
+                        os.system("pyuic %s -o %s" % (os.path.join(dirname,n),os.path.join(dirname,m.groups()[0])+'.py'))
+                        print "Building %s.py..." % os.path.join(dirname,m.groups()[0])
+                        os.system('rm %s' % os.path.join(dirname,n))
+
+os.path.walk('rep',ui_filter,None)
+"""
+
+control = {
+	'Package': 'skolesys-ui',
+	'Version': '0.5.1',
+	'Section': 'util',
+	'Priority': 'optional',
+	'Architecture': 'i386',
+	'Depends': 'python2.4, python2.4-skolesys, python2.4-qt3',
+	'Recommends': 'skolesys_ui',
+	'Maintainer': 'Jakob Simon-Gaarde <jakob@skolesys.org>',
+	'Description': 'This is the graphical administration tool for SkoleSYS linux distribution',
+	'longdesc': 
+""" The skolesys-ui package provides the nessecary graphical tools for administrating the SkoleSYS
+ distribution. The main issue here is creating users and groups, controlling permissions,
+ creating user and group spaces, registering client workstations (Windows, Linux, MacOS)
+ and registering thin client servers (LTSP).
+"""}
+
+perm = {'skolesys-ui.py': '755'}
+
+copy = {'.': '/usr/lib/skolesys-ui/'}
+
+links = {'/usr/bin/skolesys-ui': '../lib/skolesys-ui/skolesys-ui.py'}
--- trunk/buildtools/skolesys-deb/skolesys_debinfo.py
+++ trunk/buildtools/skolesys-deb/skolesys_debinfo.py
@@ -1,18 +1,21 @@
-control = \
-"""Package: python2.4-skolesys
-Version: 0.5
-Section: python
-Priority: optional
-Architecture: i386
-Depends: python2.4, python-smbpasswd, python2.4-soappy, m2crypto, python2.4-ldap
-Recommends: skolesys_ui
-Maintainer: Jakob Simon-Gaarde <jakob@skolesys.org>
-Description: This is the base control library of the SkoleSYS linux distribution
- The skolesys package provides the nessecary tools for administrating the SkoleSYS
+fetch_method = "svn"
+
+control = {
+	'Package': 'python2.4-skolesys',
+	'Version': '0.5',
+	'Section': 'python',
+	'Priority': 'optional',
+	'Architecture': 'i386',
+	'Depends': 'python2.4, python-smbpasswd, python2.4-soappy, m2crypto, python2.4-ldap',
+	'Recommends': 'skolesys_ui',
+	'Maintainer': 'Jakob Simon-Gaarde <jakob@skolesys.org>',
+	'Description': 'This is the base control library of the SkoleSYS linux distribution',
+	'longdesc': 
+""" The skolesys package provides the nessecary tools for administrating the SkoleSYS
  distribution. The main issue here is creating users and groups, controlling permissions,
  creating user and group spaces, registering client workstations (Windows, Linux, MacOS)
  and registering thin client servers (LTSP).
-"""
+"""}
 
 perm = {'soap/server.py': '755',
 	'lib/shell.py': '755'}
@@ -26,3 +29,4 @@ copy = {
 links = {
 	'/usr/sbin/ss_admin': '../lib/python2.4/site-packages/skolesys/lib/shell.py',
 	'/usr/sbin/ss_soapserver': '../lib/python2.4/site-packages/skolesys/soap/server.py'}
+
