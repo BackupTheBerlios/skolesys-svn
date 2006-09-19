803f954832dc787a221fb18ee6468424e572d686
admin <admin@f5392857-ef32-0410-a601-84722a433970>
2006-09-19 20:00:51 +0000

    Ver 0.5 first check-in

---
+++ trunk/buildtools/skolesys-cd/skolesys-ltsp_cdinfo.py
@@ -0,0 +1,20 @@
+version = "0.5"
+
+iso_base = "kubuntu-6.06.1-desktop-i386.iso"
+
+inst_manifest_excludes = \
+"""^ubiquity
+^.*lang.*-zh
+^.*lang.*-nl
+^.*lang.*-de
+^.*lang.*-fr
+^grub
+^qtparted
+^libdebian-installer
+^libdebconfclient
+^kubuntu-live
+^user-setup"""
+
+diskname = 'SkoleSYS 0.5 LTSP "Pilot" - Release i386'
+
+iso_product = 'skolesys-0.5-ltsp-i386.iso'
