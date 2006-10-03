5d2a4c237479d8aa74b950ae70d35c221b92a326
admin <admin@f5392857-ef32-0410-a601-84722a433970>
2006-10-03 23:14:32 +0000

    confhelper

--- trunk/tools/confhelper.py
+++ trunk/tools/confhelper.py
@@ -1,17 +1,20 @@
 from ConfigParser import ConfigParser
+import os
 
 def conf2dict(filename,to_lower=True):
 	conf_dict = {}
 	if os.path.exists(filename):
 		conf = ConfigParser()
 		f=open(filename)
-		conf.readfp()
+		conf.readfp(f)
 		f.close()
+	else:
+		return None
 	sections = conf.sections()
 	for sec in sections:
 		if to_lower:
 			s = sec.lower()
-		else
+		else:
 			s = sec
 		conf_dict[s] = {}
 		for var,val in conf.items(sec):
@@ -19,3 +22,4 @@ def conf2dict(filename,to_lower=True):
 				conf_dict[s][var.lower()] = val
 			else:
 				conf_dict[s][var] = val
+	return conf_dict
