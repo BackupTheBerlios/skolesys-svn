19a97d7c108f7541a1aaa0e5d04c88ecbaae4719
admin <admin@f5392857-ef32-0410-a601-84722a433970>
2006-10-04 23:55:50 +0000

    Installation configuration example

---
+++ trunk/cfmachine/example/cf_ltsp.py
@@ -0,0 +1,19 @@
+apt_source_entries = [
+	{'type':'deb','uri':'http://mainserver.skolesys.local/debian','distribution':'pilot','components':['main','nonfree']}]
+
+fstab_entries = [
+	{'sourcefs':'mainserver.skolesys.local:/skolesys','mountpoint':'/skolesys','fstype':'nfs','options':'defaults','dump':'0','fsckorder':'0'}]
+	
+packagelist_files = [
+	'default-packages','custom-packages']
+
+conf_rootdir = \
+	'rootdir'
+
+kick_daemons = [
+	'/etc/init.d/networking restart'
+	'/etc/init.d/nscd restart'
+	'/etc/init.d/dhcp3-server restart'
+	'/etc/init.d/nfs-kernel-server restart'
+	'/etc/init.d/nfs-common restart'
+	'/etc/init.d/tftpd-hpa restart']
