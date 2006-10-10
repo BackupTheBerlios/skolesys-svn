878c259fa4499936c3402ff562b1e5f167a60be4
skolesys-devel <skolesys-devel@f5392857-ef32-0410-a601-84722a433970>
2006-10-10 19:48:04 +0000

    Final split into client/mainserver parts

--- trunk/buildtools/skolesys-apt/pilot_aptinfo.py
+++ trunk/buildtools/skolesys-apt/pilot_aptinfo.py
@@ -2,7 +2,7 @@ release_info = {
 	'origin': 'SkoleSYS',
 	'label': 'SkoleSYS',
 	'suite': 'pilot',
-	'version': '0.51',
+	'version': '0.53',
 	'codename': 'pilot',
-	'description': 'SkoleSYS Pilot 0.51'}
+	'description': 'SkoleSYS Pilot 0.53'}
 
