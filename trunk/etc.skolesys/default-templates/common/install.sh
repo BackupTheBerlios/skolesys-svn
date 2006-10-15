69d74df629bc9fdc3b9b83fbd87166f35eb35c10
skolesys-devel <skolesys-devel@f5392857-ef32-0410-a601-84722a433970>
2006-10-15 22:07:29 +0000

    etc.skolesys out

--- trunk/etc.skolesys/default-templates/common/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-ss_installer mod_apt_sources cf_install
-ss_installer install_packages cf_install
-ss_installer copy_files cf_install
-ss_installer set_hostname cf_install
-ss_installer kick_daemons cf_install
-ss_installer mod_fstab cf_install
