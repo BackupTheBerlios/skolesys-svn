1560ee22a47adb1bf4c2c09e8649affd03d0a55c
skolesys-devel <skolesys-devel@f5392857-ef32-0410-a601-84722a433970>
2006-10-24 21:28:18 +0000

    Added configuration templates to repository

---
+++ trunk/config-templates/default-templates/ltspserver/cf_install.py
@@ -0,0 +1,28 @@
+apt_source_entries = [
+	{'type':'deb','uri':'http://mainserver.skolesys.local/debian','distribution':'pilot','components':['main','nonfree']},
+        {'type':'deb','uri':'http://dk.archive.ubuntu.com/ubuntu/','distribution':'dapper','components':['main','restricted','universe']},
+        {'type':'deb-src','uri':'http://dk.archive.ubuntu.com/ubuntu/','distribution':'dapper','components':['main','restricted','universe']},
+        {'type':'deb','uri':'http://dk.archive.ubuntu.com/ubuntu/','distribution':'dapper-backports','components':['main','restricted','universe','multiverse']},
+        {'type':'deb-src','uri':'http://dk.archive.ubuntu.com/ubuntu/','distribution':'dapper-backports','components':['main','restricted','universe','multiverse']},
+        {'type':'deb','uri':'http://security.ubuntu.com/ubuntu','distribution':'dapper-security','components':['main','restricted','universe']},
+        {'type':'deb-src','uri':'http://security.ubuntu.com/ubuntu','distribution':'dapper-security','components':['main','restricted','universe']}]
+
+fstab_entries = [
+	{'sourcefs':'mainserver.skolesys.local:/skolesys','mountpoint':'/skolesys','fstype':'nfs','options':'defaults','dump':'0','fsckorder':'0'}]
+	
+packagelist_files = [
+	'default-packages','custom-packages']
+
+copy_files_rootdir = \
+	'rootdir'
+
+hostname = \
+	'$reciever.hostName'
+
+kick_daemons = [
+	'/etc/init.d/networking restart',
+	'/etc/init.d/nscd restart',
+	'/etc/init.d/dhcp3-server restart',
+	'/etc/init.d/nfs-kernel-server restart',
+	'/etc/init.d/nfs-common restart',
+	'/etc/init.d/tftpd-hpa restart']
