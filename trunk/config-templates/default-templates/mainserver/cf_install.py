6389f276ef482980379dccbb4cbdfa340f7b1072
skolesys-devel <skolesys-devel@f5392857-ef32-0410-a601-84722a433970>
2006-10-25 22:31:29 +0000

    mainserver default packages template

---
+++ trunk/config-templates/default-templates/mainserver/cf_install.py
@@ -0,0 +1,28 @@
+apt_source_entries = [
+	{'type':'deb','uri':'http://mainserver.skolesys.local/debian','distribution':'pilot','components':['main','nonfree']},
+        {'type':'deb','uri':'http://dk.archive.ubuntu.com/ubuntu/','distribution':'dapper','components':['main','restricted','universe']},
+        {'type':'deb-src','uri':'http://dk.archive.ubuntu.com/ubuntu/','distribution':'dapper','components':['main','restricted','universe']},
+        {'type':'deb','uri':'http://dk.archive.ubuntu.com/ubuntu/','distribution':'dapper-backports','components':['main','restricted','universe','multiverse']},
+        {'type':'deb-src','uri':'http://dk.archive.ubuntu.com/ubuntu/','distribution':'dapper-backports','components':['main','restricted','universe','multiverse']},
+        {'type':'deb','uri':'http://security.ubuntu.com/ubuntu','distribution':'dapper-security','components':['main','restricted','universe']},
+        {'type':'deb-src','uri':'http://security.ubuntu.com/ubuntu','distribution':'dapper-security','components':['main','restricted','universe']}]
+
+fstab_entries = []
+	
+packagelist_files = [
+	'default-packages','custom-packages']
+
+copy_files_rootdir = \
+	'rootdir'
+
+hostname = \
+	'$reciever.hostName'
+
+kick_daemons = [
+	'/etc/init.d/networking restart',
+	'/etc/init.d/dhcp3-server restart',
+	'/etc/init.d/nfs-kernel-server restart',
+	'/etc/init.d/nfs-common restart',
+	'/etc/init.d/samba restart',
+	'/etc/init.d/dnsmasq restart',
+	'/etc/init.d/apache2 restart']
