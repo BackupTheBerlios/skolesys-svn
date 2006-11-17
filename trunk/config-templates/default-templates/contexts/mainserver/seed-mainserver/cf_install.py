f68921342d8fa450c770fd1968927043de2a5031
skolesys-devel <skolesys-devel@f5392857-ef32-0410-a601-84722a433970>
2006-11-17 12:34:23 +0000

    Update apt repositories from skolesys.dk/skolesys/debian to skolesys.dk/testing

--- trunk/config-templates/default-templates/contexts/mainserver/seed-mainserver/cf_install.py
+++ trunk/config-templates/default-templates/contexts/mainserver/seed-mainserver/cf_install.py
@@ -1,5 +1,5 @@
 apt_source_entries = [
-	{'type':'deb','uri':'http://skolesys.dk/skolesys/debian','distribution':'pilot','components':['main','nonfree']},
+	{'type':'deb','uri':'http://skolesys.dk/testing','distribution':'pilot','components':['main','nonfree']},
         {'type':'deb','uri':'http://dk.archive.ubuntu.com/ubuntu/','distribution':'dapper','components':['main','restricted','universe']},
         {'type':'deb-src','uri':'http://dk.archive.ubuntu.com/ubuntu/','distribution':'dapper','components':['main','restricted','universe']},
         {'type':'deb','uri':'http://dk.archive.ubuntu.com/ubuntu/','distribution':'dapper-backports','components':['main','restricted','universe','multiverse']},
--- trunk/seeder/init_mainserver.py
+++ trunk/seeder/init_mainserver.py
@@ -52,7 +52,7 @@ os.system('echo "" > /etc/apt/sources.list')
 
 # ensure some entries in sources.list
 apt_source_entries = [
-        {'type':'deb','uri':'http://skolesys.dk/skolesys/debian','distribution':'pilot','components':['main','nonfree']},
+        {'type':'deb','uri':'http://skolesys.dk/testing','distribution':'pilot','components':['main','nonfree']},
         {'type':'deb','uri':'http://dk.archive.ubuntu.com/ubuntu/','distribution':'dapper','components':['main','restricted','universe']},
         {'type':'deb-src','uri':'http://dk.archive.ubuntu.com/ubuntu/','distribution':'dapper','components':['main','restricted','universe']},
         {'type':'deb','uri':'http://dk.archive.ubuntu.com/ubuntu/','distribution':'dapper-backports','components':['main','restricted','universe','multiverse']},
