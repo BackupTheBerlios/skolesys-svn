dbaf01ecbb2e426bc58f5367cec1ab1385b59a3f
jakob <jakob@f5392857-ef32-0410-a601-84722a433970>
2007-02-11 22:07:09 +0000

    Better system string nicefier

--- trunk/tools/charmapping.py
+++ trunk/tools/charmapping.py
@@ -11,11 +11,17 @@ def system_nicefy_string(instr):
 	assemble = ''
 	for idx in xrange(len(ugly_chars)):
 		nicefied_ugly_chars = ugly_chars[idx]
+		
+		# nicefy some system viewed ugly characters
 		for ugly,nicer in replacemap.items():
 			nicefied_ugly_chars = nicefied_ugly_chars.replace(ugly,nicer)
 		nice_chars2 = c.findall(nicefied_ugly_chars)
-		nicefied_ugly_chars='z'.join(nice_chars2)
-				
+		nicefied_ugly_chars='_'.join(nice_chars2)
+		
+		# put an underscore if the ugly string was not null but no nicefication was performed
+		if nicefied_ugly_chars == '' and len(ugly_chars[idx]) > 0:
+			nicefied_ugly_chars = '_'
+			
 		assemble += nicefied_ugly_chars
 		if len(nice_chars) > idx:
 			assemble += nice_chars[idx]
