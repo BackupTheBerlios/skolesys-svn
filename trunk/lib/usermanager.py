0c7c5e549d65f88fc9ffba79931add8c9bf69f5f
skolesys-devel <skolesys-devel@f5392857-ef32-0410-a601-84722a433970>
2007-04-12 22:56:49 +0000

    Extra securitty within authorized_keys file

--- trunk/lib/usermanager.py
+++ trunk/lib/usermanager.py
@@ -180,6 +180,12 @@ class UserManager (LDAPUtil):
 		# Generate an SSH2 DSA private/public keypair and a putty ppk version
 		linux_home_path = home_path + '/.linux'
 		os.system('expect -f /etc/skolesys/keygen_expect_script %s %s' % (linux_home_path,uid) ) 
+		f=open('%s/.ssh/authorized_keys' % linux_home_path,'r')
+		pubkey = f.read()
+		f.close()
+		f=open('%s/.ssh/authorized_keys' % linux_home_path,'w')
+		f.write('no-port-forwarding,no-X11-forwarding,no-agent-forwarding,permitopen="%s:22",command="sleep 10000" %s' % (conf.get('TERMINAL_SERVICE','freenx'),pubkey))
+		f.close()
 		
 		# Deliver ownership
 		os.system('chown %d.%d %s -R -f' % (posix_uid,int(primarygid),os.path.normpath(home_path)))
