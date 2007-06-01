60fa0f0c7344f0efb144c93ad10a80fa9d69b048
skolesys-devel <skolesys-devel@f5392857-ef32-0410-a601-84722a433970>
2007-06-01 20:24:08 +0000

    configbuilder GPL in place

--- trunk/cfmachine/configbuilder.py
+++ trunk/cfmachine/configbuilder.py
@@ -1,4 +1,4 @@
-'''
+"""
 This file is part of the SkoleSYS libraries
 Copyright (C) 2007 Jakob Simon-Gaarde <info at skolesys.dk>
 
@@ -15,98 +15,6 @@ You should have received a copy of the GNU Library General Public License
 along with this library; see the file COPYING.LIB.  If not, write to
 the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 Boston, MA 02110-1301, USA.
-'''
-"""
-ConfigBuilder handles the SkoleSYS configuration template hierarchy. The configuration to be
-generated is controlled by the parameters given in the constructor. The constructor also 
-calls to the configuration builder method. So when the constructor returns the configuration
-scripts are build and ready to ship.
-
-The ConfigBuilder uses tempfile to create a temporary directory in the system's temp directory.
-The specific location of the configuration scripts that has been build can be read in the member
-tempdir.
-
-ConfigBuilder also packages the result into the file conf.tgz inside the temporary workspace
-directory so it can be marshalled easyly through some sort of network protocol - the SkoleSYS
-SOAP service and client provides automatic fetching of configuration scripts for registered
-hosts so a client can at any time fetch it's full configuration just by calling ss_getconf 
-as root.
-
-NOTE! The temporary workspace usually /tmp/skolesys_<somthing> is really temporary. It is 
-deleted by the destructor. So to fetch the result you must keep the ConfigBuilder instance
-alive long enough for you to make a copy.
-
-The template hierarchi:
-----------------------
-SkoleSYS ships with default configuration templates, but the resulting configuration scripts
-can easyly be customized. For instance if a particular organization wishes to customize the 
-/etc/hosts file generated for workstations it is done by copying the default /etc/hosts file
-into the custom-templates hierarchi and changing it there. This mechanism ensures 2 things:
-	1. The default-templates remain untouched.
-	2. SkoleSYS mainserver updates don't overwrite the organization cusomizations.
-	
-A side from the default templates there are 2 levels of cusomizations:
-	(0. default-templates)
-	 1. custom-templates (general customizations)
-	 2. host-templates (host specific customizations)
-
-Inside the default-templates and custom-templates exists a 'common' directory, a directory 
-per hosttype ('mainserver','ltspserver','workstation') and a 'contexts' directory which 
-again holds one directory per hosttype
-	example:
-	/etc/skolesys/default-templates/common
-	/etc/skolesys/default-templates/mainserver
-	/etc/skolesys/default-templates/ltspserver
-	/etc/skolesys/default-templates/workstation
-	/etc/skolesys/default-templates/contexts/mainserver
-	/etc/skolesys/default-templates/contexts/ltspserver
-
-The host-templates holds a mac-address folder per host having special configurations
-	example:
-	host-templates/01:02:03:04:05:06
-	host-templates/01:23:45:67:89:ab
-
-Distribution end points are used to target the specific Ubuntu version of the system requesting its configuration. The ss_getconf
-command on skolesys clients (ltspserver or workstation) will automatically detect the distribution codename.
-
-The configuration hierarchi is as follows for host '01:23:45:67:89:ab' with hosttype 'ltspserver' running on Ubuntu codename 'feisty':
-	default-templates/common/all
-	default-templates/common/feisty
-	default-templates/mainserver/all
-	default-templates/mainserver/feisty
-	custom-templates/common/all
-	custom-templates/common/feisty
-	custom-templates/mainserver/all
-	custom-templates/mainserver/feisty
-	host-templates/01:23:45:67:89:ab/all (you could add 'feisty' here too but there is not much point in it)
-
-So what happens is that all files files under the each of the directories above are parsed 
-recursively using python cheetah. The data used to parse the files origin partially from 
-skolesys,conf and partially from the hosts registered in the ldap back-end.
-
-Contexts:
---------
-Contexts (context argument) are used for creating specialized configurations used for certain events. 
-When in context mode it is possible to skip the basic hierarchi (from above) and just run through
-the context alone (context_only argument) in the examples below are shown a context parse with
-context_only and one in combination with the basic hierarchi.
-
-The configuration hierarchi for host '01:23:45:67:89:ab' with hosttype 'mainserver' in the 
-context 'update-hosts' (context_only=True):
-	default-templates/contexts/mainserver/update-hosts
-	custom-templates/contexts/mainserver/update-hosts
-	host-templates/contexts/01:23:45:67:89:ab/mainserver/update-hosts
-
-The configuration hierarchi for host '01:23:45:67:89:ab' with hosttype 'mainserver' in the 
-context 'update-hosts' (context_only=False):
-	default-templates/common
-	default-templates/mainserver
-	default-templates/contexts/mainserver/update-hosts
-	custom-templates/common
-	custom-templates/mainserver
-	custom-templates/contexts/mainserver/update-hosts
-	host-templates/01:23:45:67:89:ab
-	host-templates/contexts/01:23:45:67:89:ab/mainserver/update-hosts
 """
 
 from infocollection import InfoCollection
@@ -116,6 +24,99 @@ import tempfile,os
 
 
 class ConfigBuilder:
+	"""
+	ConfigBuilder handles the SkoleSYS configuration template hierarchy. The configuration to be
+	generated is controlled by the parameters given in the constructor. The constructor also 
+	calls to the configuration builder method. So when the constructor returns the configuration
+	scripts are build and ready to ship.
+	
+	The ConfigBuilder uses tempfile to create a temporary directory in the system's temp directory.
+	The specific location of the configuration scripts that has been build can be read in the member
+	tempdir.
+	
+	ConfigBuilder also packages the result into the file conf.tgz inside the temporary workspace
+	directory so it can be marshalled easyly through some sort of network protocol - the SkoleSYS
+	SOAP service and client provides automatic fetching of configuration scripts for registered
+	hosts so a client can at any time fetch it's full configuration just by calling ss_getconf 
+	as root.
+	
+	NOTE! The temporary workspace usually /tmp/skolesys_<somthing> is really temporary. It is 
+	deleted by the destructor. So to fetch the result you must keep the ConfigBuilder instance
+	alive long enough for you to make a copy.
+	
+	The template hierarchi:
+	----------------------
+	SkoleSYS ships with default configuration templates, but the resulting configuration scripts
+	can easyly be customized. For instance if a particular organization wishes to customize the 
+	/etc/hosts file generated for workstations it is done by copying the default /etc/hosts file
+	into the custom-templates hierarchi and changing it there. This mechanism ensures 2 things:
+		1. The default-templates remain untouched.
+		2. SkoleSYS mainserver updates don't overwrite the organization cusomizations.
+		
+	A side from the default templates there are 2 levels of cusomizations:
+		(0. default-templates)
+		1. custom-templates (general customizations)
+		2. host-templates (host specific customizations)
+	
+	Inside the default-templates and custom-templates exists a 'common' directory, a directory 
+	per hosttype ('mainserver','ltspserver','workstation') and a 'contexts' directory which 
+	again holds one directory per hosttype
+		example:
+		/etc/skolesys/default-templates/common
+		/etc/skolesys/default-templates/mainserver
+		/etc/skolesys/default-templates/ltspserver
+		/etc/skolesys/default-templates/workstation
+		/etc/skolesys/default-templates/contexts/mainserver
+		/etc/skolesys/default-templates/contexts/ltspserver
+	
+	The host-templates holds a mac-address folder per host having special configurations
+		example:
+		host-templates/01:02:03:04:05:06
+		host-templates/01:23:45:67:89:ab
+	
+	Distribution end points are used to target the specific Ubuntu version of the system requesting its configuration. The ss_getconf
+	command on skolesys clients (ltspserver or workstation) will automatically detect the distribution codename.
+	
+	The configuration hierarchi is as follows for host '01:23:45:67:89:ab' with hosttype 'ltspserver' running on Ubuntu codename 'feisty':
+		default-templates/common/all
+		default-templates/common/feisty
+		default-templates/mainserver/all
+		default-templates/mainserver/feisty
+		custom-templates/common/all
+		custom-templates/common/feisty
+		custom-templates/mainserver/all
+		custom-templates/mainserver/feisty
+		host-templates/01:23:45:67:89:ab/all (you could add 'feisty' here too but there is not much point in it)
+	
+	So what happens is that all files files under the each of the directories above are parsed 
+	recursively using python cheetah. The data used to parse the files origin partially from 
+	skolesys,conf and partially from the hosts registered in the ldap back-end.
+	
+	Contexts:
+	--------
+	Contexts (context argument) are used for creating specialized configurations used for certain events. 
+	When in context mode it is possible to skip the basic hierarchi (from above) and just run through
+	the context alone (context_only argument) in the examples below are shown a context parse with
+	context_only and one in combination with the basic hierarchi.
+	
+	The configuration hierarchi for host '01:23:45:67:89:ab' with hosttype 'mainserver' in the 
+	context 'update-hosts' (context_only=True):
+		default-templates/contexts/mainserver/update-hosts
+		custom-templates/contexts/mainserver/update-hosts
+		host-templates/contexts/01:23:45:67:89:ab/mainserver/update-hosts
+	
+	The configuration hierarchi for host '01:23:45:67:89:ab' with hosttype 'mainserver' in the 
+	context 'update-hosts' (context_only=False):
+		default-templates/common
+		default-templates/mainserver
+		default-templates/contexts/mainserver/update-hosts
+		custom-templates/common
+		custom-templates/mainserver
+		custom-templates/contexts/mainserver/update-hosts
+		host-templates/01:23:45:67:89:ab
+		host-templates/contexts/01:23:45:67:89:ab/mainserver/update-hosts
+	"""
+
 
 	def __init__(self,hosttype_id,dist_codename,hwaddr=None,context=None,context_only=False,pretend=False):
 		"""
