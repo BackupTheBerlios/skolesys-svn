5fbb3f4fc732f0eaa277056f5dd7a3564b17def0
skolesys-devel <skolesys-devel@f5392857-ef32-0410-a601-84722a433970>
2007-06-16 21:02:26 +0000

    Add feisty support

---
+++ trunk/buildtools/skolesys-apt/dapper_aptinfo.py
@@ -0,0 +1,8 @@
+release_info = {
+	'origin': 'SkoleSYS',
+	'label': 'SkoleSYS',
+	'suite': 'dapper',
+	'version': '0.9.5-2',
+	'codename': 'dapper',
+	'description': 'SkoleSYS for dapper 0.9.5-2'}
+
---
+++ trunk/buildtools/skolesys-apt/feisty_aptinfo.py
@@ -0,0 +1,8 @@
+release_info = {
+	'origin': 'SkoleSYS',
+	'label': 'SkoleSYS',
+	'suite': 'feisty',
+	'version': '0.9.5-2',
+	'codename': 'feisty',
+	'description': 'SkoleSYS for feisty 0.9.5-2'}
+
