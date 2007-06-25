9bc8c503f306d7b383d27551692ffff87d17e2e5
jakobsg <jakobsg@f5392857-ef32-0410-a601-84722a433970>
2007-06-25 08:48:37 +0000

    SkoleSYS documentation - disttools  * skolesys-apt

--- trunk/disttools/skolesys-apt/feisty_aptinfo.py
+++ trunk/disttools/skolesys-apt/feisty_aptinfo.py
@@ -1,8 +1,12 @@
 release_info = {
 	'origin': 'SkoleSYS',
 	'label': 'SkoleSYS',
-	'suite': 'feisty',
+	'suite': 'stable',
 	'version': '0.9.5-2',
 	'codename': 'feisty',
-	'description': 'SkoleSYS for feisty 0.9.5-2'}
+	'description': '''
+SkoleSYS for feisty 0.9.5-2. This archive contains the SkoleSYS libraries and
+a binary package of LTSP 4.2 for the i386 architecture'''}
+
+gpgid = "EEF2B7FA"
 
