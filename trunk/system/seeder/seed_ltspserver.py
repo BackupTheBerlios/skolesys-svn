51495bca634629408a53ae85e9f9d944bf717aa8
jakobsg <jakobsg@f5392857-ef32-0410-a601-84722a433970>
2007-06-26 21:11:46 +0000

    More just-in-time root-check

--- trunk/system/seeder/seed_ltspserver.py
+++ trunk/system/seeder/seed_ltspserver.py
@@ -18,4 +18,6 @@ the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 Boston, MA 02110-1301, USA.
 '''
 import init_client
-init_client.init_client('ltspserver')
+
+if __name__=='__main__':
+	init_client.init_client('ltspserver')
