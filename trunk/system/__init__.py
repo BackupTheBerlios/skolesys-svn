594cd48d141f47bd34d3acf115aab1baba48bfe0
jakobsg <jakobsg@f5392857-ef32-0410-a601-84722a433970>
2007-06-27 21:48:45 +0000

    Documentation (Reference)  * skolesys (__init__)  * skolesys.cfmachine (__init__)

--- trunk/system/__init__.py
+++ trunk/system/__init__.py
@@ -15,7 +15,63 @@
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301, USA.
 
-__all__ = ["lib","soap","cfmachine","tools","definitions","groupservices"]
+"""
+The skolesys package is used by SkoleSYS mainservers and SkoleSYS workstations
+and SkoleSYS LTSP servers. These three host types all depend on different parts
+of the package contents but not all of the contents. Therefore it kan not be
+expected that all contents are available on any given SkoleSYS seeded host.
+
+skolesys.cfmachine
+------------------
+The SkoleSYS configuration system. Any Linux based SkoleSYS host (mainserver,
+LTSP server or workstation) can request an updated configuration as long as the
+host is registered.
+cfmachine is template based. SkoleSYS comes with default templates for configuring
+the three SkoleSYS linux based host types - these templates can be found in 
+/etc/skolesys/default-templates. Here you will also find two other empty directories
+custom-templates and host-templates for general and host-specific customization.
+
+skolesys.definitions
+--------------------
+The definitions sub-package is where types are defined. While writing this there
+are 3 definition classes: user type definitions, group type definitions, host type
+definitions. There is also a class for glueing groups and users together with LDAP
+queries.
+
+skolesys.lib
+------------
+A sub-package containing the classes for controlling users, groups, services
+and hosts. The SkoleSYS mainserver uses these classes for it's main housekeeping:
+	- Create, delete and modify users
+	- Create, delete and modify groups
+	- Register SkoleSYS linux based hosts
+	
+There are also command line scripts for doing administration via a shell.
+skolesys.lib is only used by the SkoleSYS mainserver but can be manipulated by
+school administrators through the skolesys.soap interface.
+
+skolesys.services
+-----------------
+SkoleSYS services are pluggable and can be installed as add-on products. The only
+service that that is distributed with SkoleSYS is groupservice "webservice".
+services can be grouped into 2 categories groupservices and userservices. 
+
+skolesys.soap
+-------------
+Features a SOAP server that runs on the SkoleSYS mainserver, and a SOAP client
+that can run from any other Linux based SkoleSYS host type. The SOAP server
+exposes all nessecary functions needed to do "normal" administrative tasks
+remotely. The official SkoleSYS administrative GUI "skolesys-qt4" uses this
+SOAP interface to communicate with the SkoleSYS back-end. SOAPpy is used
+for the interface implementation.
+
+skolesys.tools
+--------------
+Here will be placed all ad-hoc functions.
+
+"""
+
+__all__ = ["lib","soap","cfmachine","tools","definitions","services"]
 
 def _get_exports_list(module):
     try:
--- trunk/system/cfmachine/__init__.py
+++ trunk/system/cfmachine/__init__.py
@@ -15,6 +15,48 @@
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301, USA.
 
+"""
+skolesys.cfmachine is the SkoleSYS configuration service. Any registered 
+SkoleSYS host can request its own (maybe unique) configuration.
+When a host requests an updated configuration cfmachine starts by 
+collecting all relevant information for the domain, host type and even
+the specific host. The configuration is returned as a tarball which the
+requesting host them extracts and deploys by calling the install.sh
+script enclosed in the tarball.
+
+The configuration system is template based and default templates come with
+the standard SkoleSYS installation placed under:
+  /etc/skolesys/default-templates
+
+Hosts can request their full configuration or just a part of it (by
+context). For the system to deliver the best suited configuration
+for a specific host it must send its distibution release "codename"
+(ie. dapper or feisty). This is done automatically when using the 
+ss_getconf command from a host.
+
+It is easy to add custom configurations to host-types, contexts and even
+specific hosts. ie. if we need to alter /etc/nsswitch.conf which by default
+is a common template (all host types) used by all distibution releases:
+  /etc/skolesys/default-templates/common/all/rootdir/etc/nsswitch.conf
+
+to something slightly different - it is just a question of placing a copy
+of the original in the correct directory and then make the change. 
+Here are some examples of where to place the modified nsswitch.conf:
+
+1. All host types
+  /etc/skolesys/custom-templates/common/all/rootdir/etc/nsswitch.conf
+
+2. All LTSP server host types
+  /etc/skolesys/custom-templates/ltspserver/all/rootdir/etc/nsswitch.conf
+
+3. All workstation host types using (k)ubuntu release codename "feisty"
+  /etc/skolesys/custom-templates/ltspserver/feisty/rootdir/etc/nsswitch.conf
+
+4. Special treatment for host 00:11:22:33:44:55
+  /etc/skolesys/host-templates/01:23:45:67:89:ab/all/rootdir/etc/nsswitch.conf
+
+"""
+
 __all__ = ["infocollection","configbuilder","fstabhelpers","apthelpers"]
 
 def _get_exports_list(module):
