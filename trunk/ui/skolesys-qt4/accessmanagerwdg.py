9859796c0a89dcf715783a9d8cf6970f1d9a3e78
jakobsg <jakobsg@f5392857-ef32-0410-a601-84722a433970>
2007-07-08 00:42:25 +0000

    Ups - Forgot to add the Access Manager source files...

---
+++ trunk/ui/skolesys-qt4/accessmanagerwdg.py
@@ -0,0 +1,118 @@
+# This file is part of the SkoleSYS libraries
+# Copyright (C) 2007 Jakob Simon-Gaarde <info at skolesys.dk>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License version 2 as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public License
+# along with this library; see the file COPYING.LIB.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301, USA.
+
+
+from PyQt4 import QtCore, QtGui
+import ui_accessmanagerwdg as ui_amwdg
+import usermodel as umod
+import skolesys.definitions.userdef as userdef
+import connectionmanager as cm
+import pickle
+import pyqtui4.pluggablemodelhelper as pmh
+import ss_mainwindow as mainwin
+import servermessages as srvmsg
+
+class AccessManagerWdg(QtGui.QDialog, ui_amwdg.Ui_AccessManagerWdg):
+	
+	def __init__(self,parent):
+		QtGui.QWidget.__init__(self,parent)
+		self.current_uid = None
+		self.proxy = cm.get_proxy_handle()
+		self.setupUi(self)
+		self.setupModel()
+		self.setupUserView()
+		self.setupAccessView()
+		self.updateUserView()
+		
+		self.trv_userlist.connectEvent('currentChanged',self.userChanged)
+		self.connect(self.trw_access_idents,QtCore.SIGNAL('itemChanged ( QTreeWidgetItem * , int )'),self.accessChanged)
+		self.connect(self.btn_close,QtCore.SIGNAL('clicked()'),self.accept)
+
+	def setupModel(self):
+		self.usermodel = umod.UserModel(self.trv_userlist)
+		self.umodhelper = pmh.PluggableModelHelper(self.usermodel)
+		self.umodhelper.setView(self.trv_userlist)
+		for colidx in xrange(self.usermodel.columnCount()):
+			self.umodhelper.setColumnReadOnly(colidx)
+
+	def setupUserView(self):
+		self.trv_userlist.setColumnHidden(self.usermodel.columninfo['uidnumber']['columnindex'],True)
+		self.trv_userlist.setColumnHidden(self.usermodel.columninfo['uid']['columnindex'],True)
+		self.trv_userlist.setColumnHidden(self.usermodel.columninfo['usertype']['columnindex'],True)
+
+
+	def setupAccessView(self):
+		self.trw_access_idents.setColumnCount(1)
+		self.trw_access_idents.setHeaderLabels([self.tr('Access')])
+		proxy = cm.get_proxy_handle()
+		access_idents = proxy.list_access_identifiers()
+		access_groups = {}
+		translator = srvmsg.get_translator()
+		for ai in access_idents:
+			ai_parts = ai.split('.')
+			if not access_groups.has_key(ai_parts[0]):
+				access_groups[ai_parts[0]] = {
+					'access_idents':[],
+					'text': translator.q_tr('access','%s_description' % ai_parts[0])}
+			access_groups[ai_parts[0]]['access_idents'] += [{
+				'access_ident': ai,
+				'text':translator.q_tr('access','%s_infinitive' % ai) }]
+		
+		for grp,ai_list in access_groups.items():
+			grp_item = QtGui.QTreeWidgetItem(self.trw_access_idents,[ai_list['text']])
+			self.trw_access_idents.addTopLevelItem(grp_item)
+			for access_ident in ai_list['access_idents']:
+				ai_item = QtGui.QTreeWidgetItem(grp_item,[access_ident['text']])
+				ai_item.setCheckState(0,QtCore.Qt.Unchecked)
+				ai_item.setData(0,32,QtCore.QVariant(access_ident['access_ident']))
+				grp_item.addChild(ai_item)
+			self.trw_access_idents.expandItem(grp_item)
+		
+		self.trw_access_idents.setEnabled(False)
+			
+		
+	def updateUserView(self):
+		self.usermodel.loadUsers()
+			
+		for colidx in xrange(self.usermodel.columnCount()):
+			self.trv_userlist.resizeColumnToContents(colidx)
+			
+	def userChanged(self,sender,idx):
+		self.disconnect(self.trw_access_idents,QtCore.SIGNAL('itemChanged ( QTreeWidgetItem * , int )'),self.accessChanged)
+		uidnumber = idx.data(QtCore.Qt.UserRole).toInt()[0]
+		uid = self.usermodel.users[uidnumber]['uid']
+		self.current_uid = uid
+		permlist = cm.get_proxy_handle().list_permissions(uid)
+		for idx in xrange(self.trw_access_idents.topLevelItemCount()):
+			topitem = self.trw_access_idents.topLevelItem(idx)
+			for cidx in xrange(topitem.childCount()):
+				childitem = topitem.child(cidx)
+				if permlist.count(str(childitem.data(0,32).toString())):
+					childitem.setCheckState(0,QtCore.Qt.Checked)
+				else:
+					childitem.setCheckState(0,QtCore.Qt.Unchecked)
+		self.trw_access_idents.setEnabled(True)
+		self.connect(self.trw_access_idents,QtCore.SIGNAL('itemChanged ( QTreeWidgetItem * , int )'),self.accessChanged)
+	
+	def accessChanged(self,item):
+		proxy = cm.get_proxy_handle()
+		access_ident = str(item.data(0,32).toString())
+		if item.checkState(0)==QtCore.Qt.Checked:
+			proxy.grant_access(self.current_uid,access_ident)
+		else:
+			proxy.revoke_access(self.current_uid,access_ident)
+		
---
+++ trunk/ui/skolesys-qt4/ui_accessmanagerwdg.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ui_accessmanagerwdg.ui'
+#
+# Created: Sun Jul  8 02:19:56 2007
+#      by: PyQt4 UI code generator 4.1
+#
+# WARNING! All changes made in this file will be lost!
+
+import sys
+from PyQt4 import QtCore, QtGui
+
+class Ui_AccessManagerWdg(object):
+    def setupUi(self, AccessManagerWdg):
+        AccessManagerWdg.setObjectName("AccessManagerWdg")
+        AccessManagerWdg.resize(QtCore.QSize(QtCore.QRect(0,0,709,563).size()).expandedTo(AccessManagerWdg.minimumSizeHint()))
+
+        self.gridlayout = QtGui.QGridLayout(AccessManagerWdg)
+        self.gridlayout.setMargin(9)
+        self.gridlayout.setSpacing(6)
+        self.gridlayout.setObjectName("gridlayout")
+
+        self.splitter = QtGui.QSplitter(AccessManagerWdg)
+        self.splitter.setOrientation(QtCore.Qt.Horizontal)
+        self.splitter.setObjectName("splitter")
+
+        self.layoutWidget = QtGui.QWidget(self.splitter)
+        self.layoutWidget.setObjectName("layoutWidget")
+
+        self.vboxlayout = QtGui.QVBoxLayout(self.layoutWidget)
+        self.vboxlayout.setMargin(0)
+        self.vboxlayout.setSpacing(6)
+        self.vboxlayout.setObjectName("vboxlayout")
+
+        self.label_2 = QtGui.QLabel(self.layoutWidget)
+        self.label_2.setObjectName("label_2")
+        self.vboxlayout.addWidget(self.label_2)
+
+        self.trv_userlist = EnhancedTreeView(self.layoutWidget)
+
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Policy(1),QtGui.QSizePolicy.Policy(7))
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.trv_userlist.sizePolicy().hasHeightForWidth())
+        self.trv_userlist.setSizePolicy(sizePolicy)
+        self.trv_userlist.setAlternatingRowColors(True)
+        self.trv_userlist.setRootIsDecorated(False)
+        self.trv_userlist.setObjectName("trv_userlist")
+        self.vboxlayout.addWidget(self.trv_userlist)
+
+        self.layoutWidget1 = QtGui.QWidget(self.splitter)
+        self.layoutWidget1.setObjectName("layoutWidget1")
+
+        self.vboxlayout1 = QtGui.QVBoxLayout(self.layoutWidget1)
+        self.vboxlayout1.setMargin(0)
+        self.vboxlayout1.setSpacing(6)
+        self.vboxlayout1.setObjectName("vboxlayout1")
+
+        self.label = QtGui.QLabel(self.layoutWidget1)
+        self.label.setObjectName("label")
+        self.vboxlayout1.addWidget(self.label)
+
+        self.trw_access_idents = QtGui.QTreeWidget(self.layoutWidget1)
+
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Policy(3),QtGui.QSizePolicy.Policy(7))
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.trw_access_idents.sizePolicy().hasHeightForWidth())
+        self.trw_access_idents.setSizePolicy(sizePolicy)
+        self.trw_access_idents.setAlternatingRowColors(True)
+        self.trw_access_idents.setSelectionMode(QtGui.QAbstractItemView.NoSelection)
+        self.trw_access_idents.setObjectName("trw_access_idents")
+        self.vboxlayout1.addWidget(self.trw_access_idents)
+        self.gridlayout.addWidget(self.splitter,0,0,1,1)
+
+        self.hboxlayout = QtGui.QHBoxLayout()
+        self.hboxlayout.setMargin(0)
+        self.hboxlayout.setSpacing(6)
+        self.hboxlayout.setObjectName("hboxlayout")
+
+        spacerItem = QtGui.QSpacerItem(391,20,QtGui.QSizePolicy.Expanding,QtGui.QSizePolicy.Minimum)
+        self.hboxlayout.addItem(spacerItem)
+
+        self.btn_close = QtGui.QPushButton(AccessManagerWdg)
+        self.btn_close.setObjectName("btn_close")
+        self.hboxlayout.addWidget(self.btn_close)
+        self.gridlayout.addLayout(self.hboxlayout,1,0,1,1)
+
+        self.retranslateUi(AccessManagerWdg)
+        QtCore.QMetaObject.connectSlotsByName(AccessManagerWdg)
+
+    def retranslateUi(self, AccessManagerWdg):
+        AccessManagerWdg.setWindowTitle(QtGui.QApplication.translate("AccessManagerWdg", "Access Manager", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_2.setText(QtGui.QApplication.translate("AccessManagerWdg", "Users:", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("AccessManagerWdg", "Permissions:", None, QtGui.QApplication.UnicodeUTF8))
+        self.btn_close.setText(QtGui.QApplication.translate("AccessManagerWdg", "Close", None, QtGui.QApplication.UnicodeUTF8))
+
+from pyqtui4.enhancedtreeview import EnhancedTreeView
