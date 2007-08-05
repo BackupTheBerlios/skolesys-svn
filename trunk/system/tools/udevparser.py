15758115a3dc137a8373abd0c061ee84625a0b3b
jakobsg <jakobsg@f5392857-ef32-0410-a601-84722a433970>
2007-08-05 19:10:29 +0000

    skolesys.tools: * Added script notifier in udevparser

--- trunk/system/tools/udevparser.py
+++ trunk/system/tools/udevparser.py
@@ -29,10 +29,18 @@ def reset_partition_props():
 	props = {'PARTNO' : '1',
 		'MNT_POINT' : None,
 		'ADD_SCRIPT' : None,
-		'REMOVE_SCRIPT' : None }
+		'ADD_SCRIPT_NOTIFY': None,
+		'ADD_SCRIPT_SUBJECT': 'ADD_SCRIPT Notifier',
+		'ADD_SCRIPT_BODYFILE': None,
+		'ADD_SCRIPT_SENDER': 'anonymous@domain.org',
+		'REMOVE_SCRIPT' : None,
+		'REMOVE_SCRIPT_NOTIFY': None,
+		'REMOVE_SCRIPT_SUBJECT': 'REMOVE_SCRIPT Notifier',
+		'REMOVE_SCRIPT_BODYFILE': None,
+		'REMOVE_SCRIPT_SENDER': 'anonymous@domain.org' }
 
 conf = ConfigParser.ConfigParser()
-conf.read('/etc/skolesys/backup.conf')
+conf.read('/home/skolesys/backup.conf')
 sections = conf.sections()
 
 rx_partno = re.compile('.*([0-9]+)')
@@ -76,6 +84,23 @@ for sec in sections:
 				res = os.system(props['ADD_SCRIPT'])
 				log('Script exited with %d' % res)
 
+				if props['ADD_SCRIPT_NOTIFY']:
+					log('Notifying: %s' % props['ADD_SCRIPT_NOTIFY'] )
+					f = open('/tmp/add_script_notification','w')
+					f.write('From: %s\n' % props['ADD_SCRIPT_SENDER'])
+					f.write('Subject: %s\n' % props['ADD_SCRIPT_SUBJECT'])
+					f.write('To: %s\n\n' % props['ADD_SCRIPT_NOTIFY'])
+					if props['ADD_SCRIPT_BODYFILE']:
+						bodyfile = props['ADD_SCRIPT_BODYFILE']
+						if os.path.exists(bodyfile):
+							f2 = open(bodyfile)
+							f.writelines(f2.readlines())
+							f2.close()
+					f.close()
+					os.system('sendmail -t < /tmp/add_script_notification')
+					os.remove('/tmp/add_script_notification')
+					
+
 		if action=='remove':
 			if props['MNT_POINT']:
 				log('Unmounting %s' % devname)
@@ -85,3 +110,18 @@ for sec in sections:
 				log('Starting script %s' % props['REMOVE_SCRIPT'])
 				res = os.system(props['REMOVE_SCRIPT'])
 				log('Script exited with %d' % res)
+
+				if props['REMOVE_SCRIPT_NOTIFY']:
+					f = open('/tmp/remove_script_notification','w')
+					f.write('From: %s\n' % props['REMOVE_SCRIPT_SENDER'])
+					f.write('Subject: %s\n' % props['REMOVE_SCRIPT_SUBJECT'])
+					f.write('To: %s\n\n' % props['REMOVE_SCRIPT_NOTIFY'])
+					if props['REMOVE_SCRIPT_BODYFILE']:
+						bodyfile = props['REMOVE_SCRIPT_BODYFILE']
+						if os.path.exists(bodyfile):
+							f2 = open(bodyfile)
+							f.writelines(f2.readlines())
+							f2.close()
+					f.close()
+					os.system('sendmail -t < /tmp/remove_script_notification')
+					os.remove('/tmp/remove_script_notification')
