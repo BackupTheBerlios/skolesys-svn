dd74673bb918c890d68b389e80201d3d5ace0fa1
jakobsg <jakobsg@f5392857-ef32-0410-a601-84722a433970>
2007-08-05 22:57:56 +0000

    skolesys.config-templates: * mainserver - kick udev

--- trunk/system/config-templates/default-templates/contexts/mainserver/seed-mainserver/dapper/cf_install.py
+++ trunk/system/config-templates/default-templates/contexts/mainserver/seed-mainserver/dapper/cf_install.py
@@ -26,4 +26,5 @@ kick_daemons = [
 	'/etc/init.d/dnsmasq restart',
 	'/etc/init.d/apache2 restart',
 	'/etc/init.d/firestarter restart',
-	'/etc/init.d/skolesysd restart']
+	'/etc/init.d/skolesysd restart',
+	'/etc/init.d/udev restart']
--- trunk/system/config-templates/default-templates/mainserver/dapper/cf_install.py
+++ trunk/system/config-templates/default-templates/mainserver/dapper/cf_install.py
@@ -26,4 +26,5 @@ kick_daemons = [
 	'/etc/init.d/samba restart',
 	'/etc/init.d/dnsmasq restart',
 	'/etc/init.d/apache2 restart',
-	'/etc/init.d/skolesysd restart']
+	'/etc/init.d/skolesysd restart',
+	'/etc/init.d/udev restart']
